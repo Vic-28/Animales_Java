e78d5f86e51011911501fdb50bd73deb9c8dba89
Vic-28 <delgadobatistavictor@gmail.com>
2024-02-08 17:52:37 +0000

    Cambio POM

--- src/main/java/org/example/ejercicio5animales/Ejercicio5AnimalesApplication.java
+++ src/main/java/org/example/ejercicio5animales/Ejercicio5AnimalesApplication.java
@@ -2,12 +2,18 @@ package org.example.ejercicio5animales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Ejercicio5AnimalesApplication {
+public class Ejercicio5AnimalesApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Ejercicio5AnimalesApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Ejercicio5AnimalesApplication.class);
+    }
 }
